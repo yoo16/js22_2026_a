6a82d12ec06ab386c7fa94b1567a9dfd84e8bdf5
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-04-23 11:40:42 +0900

    update

--- 02_jquery_2/js/main.js
+++ 02_jquery_2/js/main.js
@@ -25,6 +25,7 @@ $(() => {
     title = '👕 Spring Apparel';
     // TODO:  id=shop-title のタイトルの設定: text()
     $('#shop-title').text(title)
+    // document.getElementById("shop-title").textContent = title;
 
     title = `👕 Spring Apparel <span class='text-sm text-gray-400'>Men & Women Edition</span>`;
     // TODO: id=shop-title のタイトルの設定: html()
